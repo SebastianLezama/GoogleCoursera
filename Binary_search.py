10a3a05b30904eec730e882359579027d0af0602
Sebastian Lezama <sebastian.lezama@gmail.com>
2022-05-23 18:56:33 -0300

    Updated shifted_search function.
    It works with given array but should not make another list, should try
    to use differential in search formula, to avoid extra RAM usage and to
    work with any given shifted array.

--- Binary_search.py
+++ Binary_search.py
@@ -2,7 +2,7 @@
 # if not it should return -1.
 
 array = [-2, 3, 4, 7, 8, 9, 11, 13]
-target = 9
+target = 3
 
 
 def search(list, number):
@@ -27,10 +27,20 @@ shifted_array = [8, 9, 11, 13, -2, 3, 4, 7]
 def shifted_search(list, number):
     left = 0
     right = len(list) - 1
-    if list[left] < list[right]:
-        search(list, number)
+    while list[left] < list[right]:
+        return search(list, number)
+    while list[left] > list[right]:
+        middle = int((left + right) / 2)
+        if list[middle] == number:
+            return middle
+        elif list[middle] > list[right]:
+            left = middle + 1
+            if list[left] < list[middle]:
+                new_list = list[left : len(list)] + list [0 : left]
+                return search(new_list, number) + left
+        
 
 
 
 if __name__ == '__main__':
-    print(shifted_search(array, target))
+    print(shifted_search(shifted_array, target))
