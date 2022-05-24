3e414e1ddecb8da3721dec13cadabe438dd97c4c
Sebastian Lezama <sebastian.lezama@gmail.com>
2022-05-23 21:25:34 -0300

    Working Shifted search function
    Long implementation, should try dividing the array first.

--- Binary_search.py
+++ Binary_search.py
@@ -1,8 +1,7 @@
 # binary search that returns index of the target number in the sorted array.
 # if not it should return -1.
 
-array = [-2, 3, 4, 7, 8, 9, 11, 13]
-target = 3
+
 
 
 def search(list, number):
@@ -13,15 +12,31 @@ def search(list, number):
         if list[middle] == number:
             return middle
         elif number < list[middle]:
-            right = middle - 1
+            right = middle
+            print(str(right) + 'R')
         else:
-            left = middle + 1
+            if number == list[right]:
+                return right
+            left = middle
+            print(str(left) + 'L')
     return -1
 
 
 # solve a shifted binary search
 
-shifted_array = [8, 9, 11, 13, -2, 3, 4, 7]
+shifted_array1 = [-2, 3, 4, 7, 8, 9, 11, 13]
+shifted_array2 = [13, -2, 3, 4, 7, 8, 9, 11]
+shifted_array3 = [11, 13, -2, 3, 4, 7, 8, 9]
+shifted_array4 = [9, 11, 13, -2, 3, 4, 7, 8]
+shifted_array5 = [8, 9, 11, 13, -2, 3, 4, 7]
+shifted_array6 = [7, 8, 9, 11, 13, -2, 3, 4]
+shifted_array7 = [4, 7, 8, 9, 11, 13, -2, 3]
+shifted_array8 = [3, 4, 7, 8, 9, 11, 13, -2]
+
+
+
+array = [-2, 3, 4, 7, 8, 9, 11, 13]
+target = 13
 
 
 def shifted_search(list, number):
@@ -34,13 +49,27 @@ def shifted_search(list, number):
         if list[middle] == number:
             return middle
         elif list[middle] > list[right]:
+            print('A')
+            if number == list[left]:
+                    return left
             left = middle + 1
             if list[left] < list[middle]:
-                new_list = list[left : len(list)] + list [0 : left]
+                new_list = list[left :] + list [0 : left]
+                print(new_list)
                 return search(new_list, number) + left
-        
-
+        elif list[middle] < list[right]:
+            print('B')
+            if number == list[left]:
+                    return left
+            right = middle - 1
+            if number == list[right]:
+                    return right
+            if list[right] > list[middle]:
+                new_list = list[middle :] + list [: middle]
+                print(new_list)
+                print(right)
+                return search(new_list, number) - right
 
 
 if __name__ == '__main__':
-    print(shifted_search(shifted_array, target))
+    print(shifted_search(shifted_array3, target))
