caa0f68c40886d73acce587d9550b9e403fcfdd3
Sebastian Lezama <sebastian.lezama@gmail.com>
2022-05-25 21:05:43 -0300

    Made changes to Fc
    Cleaned up Fc and made larger sample array to test speed.

--- Find_pivot.py
+++ Find_pivot.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 
 shifted_array1 = [13, -2, 3, 4, 7, 8, 9, 11]
 shifted_array2 = [11, 13, -2, 3, 4, 7, 8, 9]
@@ -6,42 +8,42 @@ shifted_array4 = [8, 9, 11, 13, -2, 3, 4, 7]
 shifted_array5 = [7, 8, 9, 11, 13, -2, 3, 4]
 shifted_array6 = [4, 7, 8, 9, 11, 13, -2, 3]
 shifted_array7 = [3, 4, 7, 8, 9, 11, 13, -2]
-#                L           M           R
+shifted_array8 = [x for x in range(10001, 30000001)] + [x for x in range(1, 10001)]
 
+#                L           M           R
 
 
 def find_pivot(list):
     left = 0
     right = len(list) - 1
-
+    mid = 0
     while left <= right:
-        mid = (left + right) // 2
         
         if list[mid] <= list[mid - 1]:
             return mid
         if list[mid] > list[mid + 1]:
             return mid + 1
+        
+        mid = (left + right) // 2
+
             
         # Left side of the array
-        if list[right] < list[left] > list[mid]:
-            print('A')
-            right = mid - 1
-            if list[right] > list[mid]:
-                print('a')
-                return mid  
+        if list[right] < list[mid]:
+                # print('A')
+                left = mid + 1
+               
+        
         else:
         # Right side of the array
-            if list[left] > list[right] < list[mid]:
-                print('B')
-                left = mid + 1
-                if list[left] < list[mid]:
-                    print('b')
-                    return mid
+            if list[left] > list[mid]:
+                # print('B')
+                right = mid - 1
+            
     return 'Fail'
 
 
-
+start = datetime.now()
 if __name__ == '__main__':
-    print(find_pivot(shifted_array5))
-        
-
+    print(shifted_array8[find_pivot(shifted_array8)])
+    print(datetime.now() - start)
+    
