e72a4a8550662a6296248892cec52a600724e99d
Sebastian Lezama <sebastian.lezama@gmail.com>
2022-05-25 21:14:51 -0300

    Small changes

--- Find_pivot.py
+++ Find_pivot.py
@@ -10,35 +10,29 @@ shifted_array6 = [4, 7, 8, 9, 11, 13, -2, 3]
 shifted_array7 = [3, 4, 7, 8, 9, 11, 13, -2]
 shifted_array8 = [x for x in range(10001, 30000001)] + [x for x in range(1, 10001)]
 
-#                L           M           R
-
 
 def find_pivot(list):
     left = 0
     right = len(list) - 1
-    mid = 0
+    
     while left <= right:
+        mid = (left + right) // 2
         
         if list[mid] <= list[mid - 1]:
             return mid
         if list[mid] > list[mid + 1]:
             return mid + 1
-        
-        mid = (left + right) // 2
-
-            
+          
         # Left side of the array
         if list[right] < list[mid]:
                 # print('A')
                 left = mid + 1
                
-        
         else:
         # Right side of the array
             if list[left] > list[mid]:
                 # print('B')
                 right = mid - 1
-            
     return 'Fail'
 
 
