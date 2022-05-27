01c4d59c0ee290a0e83edcb21d43ccb1f64c4ebb
Sebastian Lezama <sebastian.lezama@gmail.com>
2022-05-27 17:55:41 -0300

    Small changes

--- Tree_practice.py
+++ Tree_practice.py
@@ -8,3 +8,9 @@ def tree_sum(nums):
 
 if __name__ == '__main__':
     print(tree_sum(nums))
+
+new_list = [x for x in range(101, 301)] + [x for x in range(1, 101)]
+new_list2 = [x for x in range(50) if x % 2 == 0]
+
+print(new_list)
+print(new_list2)
