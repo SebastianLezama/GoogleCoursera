f00ec6b1c814de73012c03907716e10b2a17ed15
Sebastian Lezama <sebastian.lezama@gmail.com>
2022-06-06 15:25:57 -0300

    Initial commit

---
+++ IT Automation/image_batch_rotate_resize.py
@@ -0,0 +1,31 @@
+from PIL import Image
+import os
+
+source_path = 'C:\\Users\\Sebastian Lezama\\Pictures\\'
+dest_path = 'C:\\Users\\Sebastian Lezama\\Pictures\\Saved Pictures\\'
+size = (640,480)
+rotation = 90
+im_format = '.PNG'
+
+def imRotateResizeJpg(source, dest, size, rotation, format): # Rotates and resizes as PNG
+    for i in os.listdir(source):
+        out_file = str(os.path.splitext(i)[0] + format)
+        print("Filename: " + i)
+        if i != out_file:
+            try:
+                with Image.open(source + i) as im:
+                    os.chdir(dest)
+                    im.resize(size).rotate(rotation).save(out_file)
+                    print("Saved as: " + dest + out_file)
+                    print('------------------------------------')
+            except OSError:
+                print("Couldn't save file")
+                print('------------------------------------')
+
+
+def main():
+    imRotateResizeJpg(source_path, dest_path, size, rotation, im_format)
+
+
+if __name__ == '__main__':
+    main()
