88428cbaf2c75ca35fbe41122debda6add1e1bc9
Sebastian Lezama <sebastian.lezama@gmail.com>
2022-06-06 19:49:29 -0300

    Added if to check if im is in RGB mode

--- IT Automation/image_batch_rotate_resize.py
+++ IT Automation/image_batch_rotate_resize.py
@@ -4,7 +4,7 @@ import os
 source_path = 'C:\\Users\\Sebastian Lezama\\Pictures\\'
 dest_path = 'C:\\Users\\Sebastian Lezama\\Pictures\\Saved Pictures\\'
 size = (640,480)
-rotation = 90
+rotation = -90
 im_format = '.PNG'
 
 def imRotateResizeJpg(source, dest, size, rotation, format): # Rotates and resizes as PNG
@@ -16,16 +16,18 @@ def imRotateResizeJpg(source, dest, size, rotation, format): # Rotates and resiz
                 try:
                     print("Filename: " + i)
                     with Image.open(source + i) as im:
+                        if im.mode != 'RGB':
+                            im.convert('RGB')
                         os.chdir(dest)
                         im.resize(size).rotate(rotation).save(out_file)
                         print("Saved as: " + dest + out_file)
-                        print('------------------------------------')
+                        print('-' * 50)
                 except OSError:
                     print("Couldn't save file")
-                    print('------------------------------------')
+                    print('-' * 30)
             else:
                 print("File already formated")
-                print('------------------------------------')
+                print('-' * 30)
         else:
             continue
 
