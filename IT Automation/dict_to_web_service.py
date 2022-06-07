c423f907146eadcb742b112a7463b9a2bdb5087f
Sebastian Lezama <sebastian.lezama@gmail.com>
2022-06-07 16:17:40 -0300

    Small changes

--- IT Automation/dict_to_web_service.py
+++ IT Automation/dict_to_web_service.py
@@ -1,5 +1,4 @@
 import os
-from urllib import response
 import requests
 import json
 
@@ -7,47 +6,63 @@ import json
 Convert .txt files into dict; then make a http post
 """
 
-url = 'https://www.google.com'
 #data = {}
-source_path = ''
 # response = requests.get(url)
 # response.raise_for_status()
 # response = requests.post(url, json=data)
 
-people = []
 
-
-def writeToTxt():
+def write_to_txt(l):
     os.chdir(cwdir)
     with open('data_2022.json', 'w') as f:
-        json.dump(people, f, indent=2)
+        json.dump(l, f, indent=2)
 
 
-cwdir = 'c:\\Users\\Sebastian Lezama\\GoogleCoursera\\IT Automation\\Text_files\\'
-
-def readFromTxt(file, dir):
+def read_from_txt(file, dir):
     os.chdir(dir)
     with open(file, 'r') as f:
         data = json.load(f)
     return data
 
 
-def getDb(source):
-    os.chdir(source)
-    for i in os.listdir(source):
-        #resp = requests.get(url)
-        try:
-            resp = requests.post(url, json=readFromTxt(i, source))
-            print(resp.request.body)
-        except json.decoder.JSONDecodeError:
-            continue
+url = 'https://www.google.com'
+cwdir = 'c:\\Users\\Sebastian Lezama\\GoogleCoursera\\IT Automation\\Text_files\\'
+source_path = ''
 
-        #print(requests.post(url, json=readFromTxt(i)))
+
+def get_db(source): # Reads file and appends dict into list
+    os.chdir(source)
+    keys = ['title', 'name', 'date', 'feedback']
+    list_of_dict = []
+    for file in os.listdir(source):
+        print(file)
+        data_dict = {}
+        raw_feedback = []
+        with open(file, 'r', newline='') as f:
+            lines = f.read().splitlines()
+            for i in lines:
+                raw_feedback.append(i)
+            for index in range(len(raw_feedback)):
+                data_dict[keys[index]] = raw_feedback[index]
+            list_of_dict.append(data_dict)
+    return list_of_dict
+
+
+def get_post_json(list): # Converts list of dict into json
+        post_json = json.dumps(list, indent=2)
+        return post_json
+
+
+def post_request(url, d):
+    resp = requests.post(url, data=d)
+    print(resp.status_code)
+    print(resp.request.body)
 
 
 def main():
-    getDb(cwdir)
-    pass
+    list = get_db(cwdir)
+    data = get_post_json(list)
+    post_request(url, data)
 
 
 if __name__ == '__main__':
